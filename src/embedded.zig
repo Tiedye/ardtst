af0845a0cf786bfdf9b16dd951771eef21599904
Daniel Whitney <tiedye1@hotmail.com>
2023-06-08 20:00:44 -0400

    add deps

---
+++ src/embedded.zig
@@ -0,0 +1,11 @@
+const microzig = @import("microzig");
+
+// `microzig.config`: comptime access to configuration
+// `microzig.chip`: access to register definitions, generated code
+// `microzig.board`: access to board information
+// `microzig.hal`: access to hand-written code for interacting with the hardware
+// `microzig.cpu`: access to AVR5 specific functions
+
+pub fn main() !void {
+    // your program here
+}
